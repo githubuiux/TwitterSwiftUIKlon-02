095e67726172d87de7b53567cb6593150af747f1
githubuiux <uiuxista@gmail.com>
2023-04-10 12:52:31 +0300

    Initial Commit

---
+++ TwitterSwiftUIKlon/TwitterSwiftUIKlonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIKlonApp.swift
+//  TwitterSwiftUIKlon
+//
+//  Created by Levent on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIKlonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
